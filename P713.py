e2b45e31fa0ee5e416d7a562e6486ad4685d9b7c
oozk <onur.ozkan.1@gmail.com>
2020-05-06 21:27:33 +0200

    Update P713.py

--- P713.py
+++ P713.py
@@ -3,8 +3,6 @@
 ###
 # Problem 713
 # https://projecteuler.net/problem=713
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-# Find the sum of all the primes below two million.
 ###
 # Solution: O(n) time | O(1) space
 ###
